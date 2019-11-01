b73729adb4fdeb29c662fbb855cbb903bfded5ee
Na27i <oanazzi.setter@gmail.com>
2019-11-01 14:57:29 +0900

    add function of changeing board type, correct UI.

--- lifegame.js
+++ lifegame.js
@@ -1,32 +1,52 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - $("#box").height();
+canvas.width = window.innerWidth * 0.9;
+canvas.height = (window.innerHeight - $("#box_set").height()) * 0.9;
 let width = canvas.width;
 let height = canvas.height;
+ctx.strokeStyle = "#000000"
+ctx.strokeRect(0, 0 , width, height);
 
 //1辺のサイズ(デフォルトは10)
 let maxnum = 10;
 //配列サイズ(デフォルトは21)
 let arraySize = 2 * maxnum + 1;
-
-const setnum = function() {
-    ctx.clearRect(0, 0, width, height);
-    maxnum = parseInt(document.forms.id_fm1.id_maxnum_form.value);
-    arraySize = 2 * maxnum + 1;
-    init();
-    putHex(width/2,height/2);
-}
-
-const next = function(){
-    ctx.clearRect(0, 0, width, height);
-    update();
-    putHex(width/2,height/2);
-}
-
+//盤面のタイプ(四角：0　六角形：1)
+let boardType;
+//盤面とコピー用の盤面
 let table = [];
 let copy = [];
+
+//初期盤面の表示
+const settings = function(type) {
+    boardType = type;
+    ctx.clearRect(0, 0, width, height);
+    ctx.strokeRect(0, 0 , width, height);
+    maxnum = parseInt(document.forms.id_fm1.id_maxnum_form.value);
+    if(type == 1){
+        arraySize = 2 * maxnum + 1;
+        init();
+        putHex(width/2,height/2);
+    }
+    else{
+        arraySize = maxnum + 2;
+        init();
+        putSqu(width/2,height/2);
+    }
+}
+
+//次の世代
+const next = function(){
+    ctx.clearRect(0, 0, width, height);
+    ctx.strokeRect(0, 0 , width, height);
+    update();
+
+    if(boardType == 0)
+        putSqu(width/2,height/2);
+    else
+        putHex(width/2,height/2);
+}
 
 //盤面の初期化
 function init() {
@@ -35,20 +55,26 @@ function init() {
     for(let i = 0; i <= arraySize; i++) {
         let tableItem;
         let copyItem;
-
-        if(i<=maxnum){
-            tableItem = new Array(maxnum+i+1).fill(0);
-            copyItem = new Array(maxnum+i+1).fill(0);
-        }else{
-            tableItem = new Array(3*maxnum-i+1).fill(0);
-            copyItem = new Array(3*maxnum-i+1).fill(0);
-        }
+        
+        if(boardType == 0){
+            tableItem = new Array(maxnum+2).fill(0);
+            copyItem = new Array(maxnum+2).fill(0);
+        }else if(boardType == 1){
+            if(i<=maxnum){
+                tableItem = new Array(maxnum+i+1).fill(0);
+                copyItem = new Array(maxnum+i+1).fill(0);
+            }else{
+                tableItem = new Array(3*maxnum-i+1).fill(0);
+                copyItem = new Array(3*maxnum-i+1).fill(0);
+            }
+        }else
+            continue;
         table.push(tableItem);
         copy.push(copyItem);
     }
 
     for(let i=1; i<arraySize; i++){
-        for(let j=1; j<table[i].length; j++){
+        for(let j=1; j<table[i].length-1; j++){
             if(Math.floor(Math.random() * 5)==0)
                 table[i][j] = 1;
             else
@@ -70,8 +96,6 @@ function swap() {
 function check(i,j) {
     let count = 0;
 
-    if(table[i-1][j-1] == 1)
-        count++;
     if(table[i-1][j] == 1)
         count++;
     if(table[i][j-1] == 1)
@@ -82,6 +106,15 @@ function check(i,j) {
         count++;
     if(table[i+1][j+1] == 1)
         count++;
+
+    if(boardType == 0){
+        if(table[i-1][j+1] == 1)
+        count++;
+    }
+    else{
+        if(table[i-1][j-1] == 1)
+        count++;
+    }
 
     return count;
 }
@@ -138,7 +171,45 @@ function drawHex(x, y, size,color) {
         ctx.stroke();
 }
 
-//六角形を並べる
+//四角に並べる
+function putSqu(x, y) {
+    let size = 10;
+    
+    const movex = Math.sin(5*Math.PI / 3) * size;    
+    const movey = 3*Math.cos(5*Math.PI / 3) * size;
+
+    let startx = x + movex * maxnum;
+    let starty = y + ((-1)*movey * maxnum) / 2;
+
+    let tempx, tempy;
+    
+    for(let i=1; i<arraySize-1; i++){
+        for(let j=1; j<arraySize-1; j++){
+            if(j ==1){
+                if(i==1){
+                    tempx = startx;
+                    tempy = starty;
+                }else{
+                    tempy = starty + movey * (i-1);
+                    if(i % 2 == 1){
+                        tempx = startx;
+                    }else{
+                        tempx = startx + movex * (-1);
+                    }
+                }
+            }
+            
+            if(table[i][j] == 1)
+                drawHex(tempx, tempy, size, 1);
+            else
+                drawHex(tempx, tempy, size, 0);
+            tempx += (-2)*movex;
+        }
+        tempy += movey;
+    }
+}
+
+//六角形に並べる
 function putHex(x, y) {
     let size = 10;
     
